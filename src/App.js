8ae83761751e29e324ac74c0a5e676cc242f9d5a
Augusto Amaral <augusto.jm.amaral1@gmail.com>
2020-09-09 22:43:08 -0300

    error: add a message as a toasty

--- src/App.js
+++ src/App.js
@@ -1,67 +1,71 @@
 import React, { useState } from 'react';
-import {UnControlled as CodeMirror} from 'react-codemirror2'
+import { UnControlled as CodeMirror } from 'react-codemirror2'
 import { format } from 'prettier/standalone'
 import parserBabel from 'prettier/parser-babel'
+import Toastify from 'toastify-js'
 
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/dracula.css';
 import './App.css'
+import 'toastify-js/src/toastify.css'
 
 require('codemirror/mode/javascript/javascript');
 
-function App (props) {
-    const [text, setText] = useState('')
-    // let instance
-  
-    return <div className="wrapper">
-      <CodeMirror
-        value={text}
-        editorDidMount={editor => { 
-          // instance = editor
-          // setTimeout(() => {
-            // instance.markText(
-            //   {line: 0, ch: 0},
-            //   {line: 0, ch: 10},
-            //   {
-            //     css: "color: red",
-            //   }
-            // )
-          // }, 1000)
+let instance
+
+function App(props) {
+  const [text, setText] = useState('')
+
+  return <div className="wrapper">
+    <CodeMirror
+      editorDidMount={editor => {
+
+        instance = editor
+      }}
+      onChange={(editor, data, value) => {
+        setText(value)
+      }}
+      className="CodeMirror"
+      options={{
+        mode: { name: "javascript", json: true },
+        theme: 'dracula',
+        lineNumbers: true,
+        viewportMargin: Infinity,
+      }}
+    />
+    <div className="button-container">
+      <a /* eslint-disable-line */
+        href="#"
+        className="btn"
+        onClick={() => {
+          try {
+            const newText = format(text, {
+              parser: 'json',
+              printWidth: 0,
+              trailingComma: 'all',
+              plugins: [parserBabel]
+            })
+
+            instance.setValue(newText)
+          } catch (e) {
+            console.error(e)
+
+            Toastify({
+              text: "Invalid JSON",
+              duration: 3000,
+              newWindow: true,
+              gravity: "top", // `top` or `bottom`
+              position: 'right', // `left`, `center` or `right`
+              backgroundColor: "linear-gradient(to right, rgb(255, 95, 109), rgb(255, 195, 113))",
+              stopOnFocus: true, // Prevents dismissing of toast on hover
+            }).showToast();
+          }
         }}
-        onChange={(editor, data, value) => {
-          setText(value)
-        }}
-        className="CodeMirror"
-        options={{
-          mode: {name: "javascript", json: true},
-          theme: 'dracula',
-          lineNumbers: true,
-          viewportMargin: Infinity,
-        }}
-      />
-      <div className="button-container">
-        <a /* eslint-disable-line */
-          href="#"
-          className="btn"
-          onClick={() => {
-            try {
-              const newText = format(text, {
-                  parser: 'json',
-                  printWidth: 0,
-                  trailingComma: 'all',
-                  plugins: [parserBabel]
-                })
-    
-              setText(newText)
-            } catch (e) { 
-              console.error(e)
-            }
-          }}
-        >
-          <span>Prettify</span>
-        </a>
-      </div>
+      >
+        <span>Prettify</span>
+      </a>
     </div>
+  </div>
 }
 
 export default App;
